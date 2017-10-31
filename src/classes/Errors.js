83570cda0d96987d2238af16bd4bb8a3d5777103
Graham Daley <graham@daleybread.com>
2017-10-31 22:14:15 +0800

    Cosmetic changes to source

--- src/classes/Errors.js
+++ src/classes/Errors.js
@@ -7,7 +7,7 @@
 export class Errors{
 
 	//constructor
-	constructor(){
+	constructor() {
 		this.errors = {};
 	}
 
@@ -16,7 +16,7 @@ export class Errors{
 	 * @param {String} field - the name of the field you are looking for
 	 * 
 	 */
-	has(field){
+	has(field) {
 		return this.errors.hasOwnProperty(field);
 	}
 
@@ -24,7 +24,7 @@ export class Errors{
 	 * Check if there are any Errors
 	 * @returns {Boolean} boolean 
 	 */
-	any(){
+	any() {
 		return Objects.keys(this.errors) > 0;
 	}
 
@@ -33,8 +33,8 @@ export class Errors{
 	 * @param {String} field - the name of the field you are looking for
 	 * @returns if exists returns the error else returns false
 	 */
-	get(field){
-		if(this.errors[field]) return this.errors[field];
+	get(field) {
+		if (this.errors[field]) return this.errors[field];
 		return false;
 	}
 
@@ -42,15 +42,15 @@ export class Errors{
 	 * Sets the error Object
 	 * @param Errors - errors
 	 */
-	set(err){
-		this.errors=err;
+	set(err) {
+		this.errors = err;
 	}
 
 	/**
 	 * Clear all the erros
 	 */
-	clear(){
-		this.erros={};
+	clear() {
+		this.errors = {};
 	}
 
 
--- src/dynamic-input.vue
+++ src/dynamic-input.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="dynamic-input-wrapper">
-		<label  v-show="input.label" :for="input.name" >{{input.label}}</label>
+		<label v-if="input.type !== 'checkbox'" v-show="input.label" :for="input.name" >{{input.label}}</label>
 
 		<!-- error span -->
 		<div v-show="error" class="error">
@@ -12,77 +12,75 @@
 		
 		<!-- start input type if -->
 			
-			<!-- hidden input -->
-			<input v-if="input.type === 'hidden'" :class="input.class" type="hidden" :name="input.name" v-model="input.value">
+		<!-- hidden input -->
+		<input v-if="input.type === 'hidden'" :class="input.class" type="hidden" :name="input.name" v-model="input.value">
+		
+		<!-- text input -->
+		<input v-else-if="input.type === 'text'" :class="input.class" type="text" :name="input.name" v-model="input.value">
+
+		<!-- phone input -->
+		<input v-else-if="input.type === 'tel'" :class="input.class" type="tel" :name="input.name" v-model="input.value">
+
+		<!-- email input -->
+		<input v-else-if="input.type === 'email'" :class="input.class" type="email" :name="input.name" v-model="input.value">
+
+		<!-- password input -->
+		<input v-else-if="input.type === 'password'" :class="input.class" type="password" :name="input.name" v-model="input.value">
+
+		<!-- date input -->
+		<input v-else-if="input.type === 'date'" :class="input.class" type="date" :name="input.name" :min="input.min" :max="input.max" v-model="input.value">
+
+		<!-- number input -->
+		<input v-else-if="input.type === 'number'" :class="input.class" type="number" :name="input.name" :min="input.min" :max="input.max" :step="input.step" v-model="input.value">
+
+		<!-- textarea -->
+		<textarea v-else-if="input.type === 'textarea'" :class="input.class" :name="input.name" v-model="input.value" :placeholder="input.placeholder"></textarea>
+
+		<!-- select -->
+		<select v-else-if="input.type ==='select'" :class="input.class" :name="input.name" v-model="input.value" :selected="input.value">
+			<option v-for="option in input.options" :value="option.value ? option.value : option">{{option.label ? option.label : option}}</option>
+		</select>
+
+		<!-- Radio -->
+		<div v-else-if="input.type === 'radio'">
+			<h4>input.title</h4>			
+			<div  v-for="option in options">
+				<input  :class="input.class" type="radio" :name="input.name" v-model="input.value" value="option" id="option">
+				<label :for="option"></label>
+			</div>
+		</div>
+		
+		<!-- Checkbox -->
+		<div v-else-if="input.type == 'checkbox'" style="display:inline-flex">
+			<input :class="input.class" type="checkbox" style="display:inline;width:10%;float:left;margin-top:3px;" :name="input.name" v-model="input.value">
+			<label :for="input.name" style="display:inline">{{input.label}}</label>
+		</div>
+
+		<!-- Checkboxgroup -->
+		<div v-else-if="input.type === 'checkgroup'">
+			<h4>input.title</h4>
 			
-			<!-- text input -->
-			<input v-else-if="input.type === 'text'" :class="input.class" type="text" :name="input.name" v-model="input.value">
+		</div>
+		
+		<!-- multi select -->
+		<div v-else-if="input.type === 'multiselect'">
+			<multiselect v-model="input.value" :options="input.options || []" :trackBy="input.track" :multiple="true" :close-on-select="true" :clear-on-select="false" :hide-selected="true" :label="input.data_label" :show-labels="false" :placeholder="input.placeholder"></multiselect>
+		</div>
 
-			<!-- phone input -->
-			<input v-else-if="input.type === 'tel'" :class="input.class" type="tel" :name="input.name" v-model="input.value">
+		<div v-else-if="input.type === 'datepicker'" :class="input.class">
+			<date-picker :class="input.class" :option="input.option" :date="{time:input.value}" :limit="input.limit" @change="changeDate"></date-picker>
+		</div>
 
-			<!-- email input -->
-			<input v-else-if="input.type === 'email'" :class="input.class" type="email" :name="input.name" v-model="input.value">
+		<div v-else-if="input.type === 'places'">
+			<places-input :options="{ name:input.name || '',className: input.class, options:input.options}" :content="input.value" @change="changePlace"></places-input>
+		</div>
 
-			<!-- password input -->
-			<input v-else-if="input.type === 'password'" :class="input.class" type="password" :name="input.name" v-model="input.value">
-
-			<!-- date input -->
-			<input v-else-if="input.type === 'date'" :class="input.class" type="date" :name="input.name" :min="input.min" :max="input.max" v-model="input.value">
-
-			<!-- number input -->
-			<input v-else-if="input.type === 'number'" :class="input.class" type="number" :name="input.name" :min="input.min" :max="input.max" :step="input.step" v-model="input.value">
-
-			<!-- textarea -->
-			<textarea v-else-if="input.type === 'textarea'" :class="input.class" :name="input.name" v-model="input.value" :placeholder="input.placeholder"></textarea>
-
-			<!-- select -->
-			<select v-else-if="input.type ==='select'" :class="input.class" :name="input.name" v-model="input.value" :selected="input.value">
-				<option v-for="option in input.options" :value="option.value ? option.value : option">{{option.label ? option.label : option}}</option>
-			</select>
-
-			<!-- Radio -->
-			<div v-else-if="input.type === 'radio'">
-				<h4>input.title</h4>			
-				<div  v-for="option in options">
-					<input  :class="input.class" type="radio" :name="input.name" v-model="input.value" value="option" id="option">
-					<label :for="option"></label>
-				</div>
-			</div>
-			
-
-
-			<!-- Checkbox -->
-			<input v-else-if="input.type === 'checkbox'" :class="input.class" type="checkbox" :name="input.name" v-model="input.value">
-
-
-			<!-- Checkboxgroup -->
-			<div v-else-if="input.type === 'checkgroup'">
-				<h4>input.title</h4>
-				
-			</div>
-			
-			<!-- multi select -->
-			<div v-else-if="input.type === 'multiselect'">
-				<multiselect v-model="input.value" :options="input.options || []" :trackBy="input.track" :multiple="true" :close-on-select="true" :clear-on-select="false" :hide-selected="true" :label="input.data_label" :show-labels="false" :placeholder="input.placeholder"></multiselect>
-			</div>
-
-			<div v-else-if="input.type === 'datepicker'" :class="input.class">
-				<date-picker :class="input.class" :option="input.option" :date="{time:input.value}" :limit="input.limit" @change="changeDate"></date-picker>
-			</div>
-
-			<div v-else-if="input.type === 'places'">
-				<places-input :options="{ name:input.name || '',className: input.class, options:input.options}" :content="input.value" @change="changePlace"></places-input>
-			</div>
-
-			<div v-else>
-				<span>not supported</span>
-			</div>
-
+		<div v-else>
+			<span>not supported</span>
+		</div>
 
 		<!-- end input type if -->
-		
-		
+				
 	</div>
 </template>
 <script>
